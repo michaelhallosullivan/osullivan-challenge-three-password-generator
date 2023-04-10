1de23a72d8c7695f22c8400c722c247a4dc3b51d
Michael O'Sullivan <michaelhallosullivan@gmail.com>
2023-04-09 23:20:05 -0700

    created generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,47 +1,57 @@
-
-// Functional Password Length Slider
 var rangeInput = document.querySelector("#range-input");
 var rangeLabel = document.querySelector("#range-label");
-var passwordText = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate");
 
 rangeLabel.textContent = rangeInput.value;
 
-function changeLabel() {
+function changeLabel () {
   rangeLabel.textContent = rangeInput.value;
 }
 
 rangeInput.addEventListener("change" , changeLabel);
 
-// Store Characters in Arrays
-function arrayCharCodes(low, high) {
-  var array = [];
-  for (let i = low; i <= high; i++) {
-    array.push(i);
-  }
-  return array;
-}
-
-var lowercaseCharacters = arrayCharCodes(97 , 122);
-var uppercaseCharacters = arrayCharCodes(65 , 90);
-var numericCharacters = arrayCharCodes(48, 57);
-var specialCharacters = arrayCharCodes(33, 47).concat(arrayCharCodes(58,64)).concat(arrayCharCodes(91,96)).concat(arrayCharCodes(123,126));
+var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericCharacters = "0123456789";
+var specialCharacters = "#$%&()*+-=<>?@!";
 
 var includeLowercase = document.querySelector("#lower-check").checked;
 var includeUppercase = document.querySelector("#upper-check").checked;
-var includeNumeric = docunent.querySelector("#numeric-check").checked;
+var includeNumeric = document.querySelector("#numeric-check").checked;
 var includeSpecial = document.querySelector("#special-check").checked;
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-
-  passwordText.value = password;
-
+function generatePassword() {
+  var passwordCharacters = "";
+  if (includeLowercase === false && includeUppercase === false && includeNumeric === false && includeSpecial === false) {
+    alert("Please make at least 1 character selection.");
+    }
+  else {
+    if (includeLowercase === true) {
+    passwordCharacters = passwordCharacters.concat(lowercaseCharacters);
+    }
+    else if (includeUppercase === true) {
+    passwordCharacters = passwordCharacters.concat(uppercaseCharacters);
+    }
+    else if (includeSpecial === true) {
+    passwordCharacters = passwordCharacters.concat(specialCharacters);
+    }
+    else if (includeNumeric === true) {
+    passwordCharacters = passwordCharacters.concat(numericCharacters);
+    }
+    var generatedPassword = "";
+    for (let i=0; i <= rangeInput.value; i++) {
+    var randomCharacter = passwordCharacters.charAt[Math.floor(Math.random() * passwordCharacters.length)];
+    generatedPassword = generatedPassword.concat(randomCharacter);
+    }
+    return generatedPassword;
+  }
 }
 
-// Add event listener to generate button
+function writePassword(event) {
+  event.preventDefault();
+  var passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  passwordText.value = password;
+}
+
 generateBtn.addEventListener("click", writePassword);
